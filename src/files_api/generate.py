cff581fe96bb27457903e8f0d4762d1864d6752f
Amit Vikram Raj <avr13405@gmail.com>
2024-07-18 17:07:09 +0530

    removed commented out code

--- src/files_api/generate.py
+++ src/files_api/generate.py
@@ -10,15 +10,11 @@ from openai import AsyncOpenAI
 from openai.types.chat import ChatCompletion
 
 SYSTEM_PROMPT = "You are an autocompletion tool that produces text files given constraints."
-# prompt = "toml file representing a setuptools python package"
 
 
 def get_openai_client() -> AsyncOpenAI:
-    # point to a local mock of OpenAI
-    # if os.getenv("OPENAI_API_KEY") is None:
-    #     client = AsyncOpenAI(base_url="http://localhost:1080", api_key="mocked_key")
-    #     return client
-
+    # point to a local mock of OpenAI, se these env vars in your .env file while testing
+    # base_url="http://localhost:1080", api_key="mocked_key"
     base_url = os.getenv("OPENAI_BASE_URL")
     api_key = os.getenv("OPENAI_API_KEY")
 
@@ -88,53 +84,3 @@ async def generate_text_to_speech(
     file_mime_type: str = audio_response.headers.get("Content-Type")
 
     return file_content_bytes, file_mime_type
-
-
-# import io
-# import tempfile
-# import aiofiles
-
-# async def generate_text_to_speech_with_memory(prompt: str, openai_client: Optional[AsyncOpenAI] = None) -> bytes:
-#     # get the OpenAI client
-#     client = openai_client or get_openai_client()
-
-#     # Create a in-memory audio buffer
-#     audio_buffer = io.BytesIO()
-
-#     # Get audio response from OpenAI and write directly to the in-memory buffer
-#     async with client.audio.speech.with_streaming_response.create(
-#         model="tts-1", voice="echo", input=prompt
-#     ) as response:
-#         async for chunk in response.iter_bytes():
-#             audio_buffer.write(chunk)
-
-#     # Get the audio content as bytes
-#     file_content_bytes = audio_buffer.getvalue()
-
-#     return file_content_bytes
-
-
-# async def generate_text_to_speech_with_tempfile(prompt: str, openai_client: Optional[AsyncOpenAI] = None) -> bytes:
-#     # get the OpenAI client
-#     client = openai_client or get_openai_client()
-
-#     # Create a temporary file to store the audio
-#     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3", dir="/tmp") as temp_file:
-#         temp_audio_file = temp_file.name
-
-#     # get audio response from OpenAI
-#     async with client.audio.speech.with_streaming_response.create(
-#         model="tts-1", voice="echo", input=prompt
-#     ) as response:
-#         async with aiofiles.open(temp_audio_file, "wb") as f:
-#             async for chunk in response.iter_bytes():
-#                 await f.write(chunk)
-
-#     # Read the file content and return as bytes
-#     async with aiofiles.open(temp_audio_file, "rb") as f:
-#         file_content_bytes = await f.read()
-
-#     # Delete the temporary file
-#     os.unlink(temp_audio_file)
-
-#     return file_content_bytes
