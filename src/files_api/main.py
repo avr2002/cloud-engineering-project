9d5952e0e63cbbe1ca3784bafd3f25d4f7c7eb1e
Amit Vikram Raj <avr13405@gmail.com>
2024-12-02 22:50:58 +0530

    updated route handler to log request and response info

--- src/files_api/main.py
+++ src/files_api/main.py
@@ -62,8 +62,8 @@ def create_app(settings: Union[Settings, None] = None) -> FastAPI:
     # these middlewares get executed in reverse order that they are added to the app
     app.middleware("http")(handle_broad_exceptions__middleware)  # last middleware
     app.middleware("http")(inject_lambda_context__middleware)
-    app.middleware("http")(start_metrics_context__middleware)
-    app.middleware("http")(start_xray_tracing__middleware)  # first middleware
+    app.middleware("http")(start_xray_tracing__middleware)
+    app.middleware("http")(start_metrics_context__middleware)   # first middleware
     return app
 
 
--- src/files_api/route_handler.py
+++ src/files_api/route_handler.py
@@ -8,7 +8,11 @@ from fastapi import (
 from fastapi.routing import APIRoute
 from loguru import logger
 
-from files_api.metrics import metrics_ctx
+from files_api.monitoring.logger import (
+    log_request_info,
+    log_response_info,
+)
+from files_api.monitoring.metrics import metrics_ctx
 
 # Global variable to track cold starts
 cold_start = True
@@ -17,7 +21,7 @@ cold_start = True
 def log_lambda_cold_start():
     """Log the cold start of the Lambda function."""
     global cold_start
-    logger.info("Cold Start", **{"cold_start": cold_start})
+    logger.info(f"Cold Start: {cold_start}", **{"cold_start": cold_start})
     cold_start = False
 
 
@@ -28,19 +32,27 @@ class RouteHandler(APIRoute):
         original_route_handler = super().get_route_handler()
 
         async def route_handler(request: Request) -> Response:
-            # Add fastapi context to logs
-            context = {
+            # Add request context to all logs
+            request_context = {
                 "path": request.url.path,
                 "route": self.path,
                 "method": request.method,
             }
-            
+            logger.configure(extra={"http": request_context})
+
             # Add metrics context to logs
             metrics: MetricsLogger = metrics_ctx.get()
-            metrics.set_property(key="fastapi", value=context)
-            
-            with logger.contextualize(fastapi=context):
-                log_lambda_cold_start()
-                return await original_route_handler(request)
+
+            # @TODO: Trying to see difference b/w adding dimensions and properties; Remove any one of them later
+            # READ MORE about dimensions and properties in AWS Embedded Metrics: https://github.com/awslabs/aws-embedded-metrics-python
+            metrics.put_dimensions(request_context)
+            # metrics.set_property(key="fastapi", value=context)
+
+            # with logger.contextualize(http=request_context):
+            log_lambda_cold_start()
+            log_request_info(request)
+            response: Response = await original_route_handler(request)
+            log_response_info(response)
+            return response
 
         return route_handler
